f2d23454f8af001582d9a8acc6a282cc97d9bf2b
Joseph Abel <josephabel540@gmail.com>
2025-11-20 23:50:31 +0100

    commit ppw

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -11,7 +10,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}']
       },
-      includeAssets: ['favicon.png', 'logo.png', 'logo 2.jpg'],
+      includeAssets: ['favicon.png', 'logo.jpg', 'logo 2.jpg'],
       manifest: {
         name: 'Dewgates Consults',
         short_name: 'Dewgates',
@@ -22,14 +21,14 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/logo.png',
+            src: '/logo.jpg',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/jpg'
           },
           {
-            src: '/logo.png',
+            src: '/logo 5.jpg',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/jpg'
           }
         ]
       }
