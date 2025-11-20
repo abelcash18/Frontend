8414c495bf6934b388cbd74c2dcadea277f1a962
Joseph Abel <josephabel540@gmail.com>
2025-11-20 23:36:09 +0100

    commit pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}']
+      },
+      includeAssets: ['favicon.png', 'logo.png', 'logo 2.jpg'],
+      manifest: {
+        name: 'Dewgates Consults',
+        short_name: 'Dewgates',
+        description: 'Buy, Sell and Rent Properties',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     proxy: {
       '/auth': 'http://localhost:8800'
